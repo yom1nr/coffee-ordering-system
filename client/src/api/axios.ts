917a7696cd545d2c14d10866a8f18e9f9d2f9ba5
yom1nr <hotest224e@gmail.com>
2026-02-07 00:36:11 +0700

    Hardcode API URL to fix connection

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://coffee-ordering-system.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
